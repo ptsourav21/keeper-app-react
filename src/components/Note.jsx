2a9ad3817be9ebfef050edc3047fe0c090e462aa
Purnendu Talukder <ptsourav21@gmail.com>
2023-10-07 04:12:24 +0600

    skeleton added without functionality

---
+++ src/components/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Lorem ipsum dolor sit amet.</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium,
+        aut?
+      </p>
+    </div>
+  );
+}
+
+export default Note;
