80143e6711a79c9bf1428ae7559c61d70f6bfa72
Purnendu Talukder <ptsourav21@gmail.com>
2023-10-08 01:32:53 +0600

    update

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./components/App";
 
-ReactDom.render(<App />, document.getElementById("root"));
 
+ReactDom.render(<App />, document.getElementById("root"));
 
